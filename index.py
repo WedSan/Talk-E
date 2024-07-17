3ef8c009914eda7a9b883d421019b66c989b2f4c
sabrinacouto <sabrinacoutolima@gmail.com>
2024-07-16 22:58:21 -0300

    integrating speech-to-text with azure

--- index.py
+++ index.py
@@ -1,16 +1,28 @@
-import speech_recognition as sr
+import os
+import azure.cognitiveservices.speech as speechsdk
 
-microfone = sr.Recognizer()
+def recognize_from_microphone():
+    speech_config = speechsdk.SpeechConfig(subscription='e345dc2c285140f6b67b5c9a75e35467', region='brazilsouth')
+    speech_config.speech_recognition_language="pt-br"
 
-with sr.Microphone() as source:
-    microfone.adjust_for_ambient_noise(source)
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    print("dizer alguma coisa")
+    print("Speak into your microphone.")
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
 
-    audio = microfone.listen(source)
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(speech_recognition_result.text))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
 
-    try:
-        frase = microfone.recognize_google_cloud(audio, language='pt-br')
-        print("frase: "+frase)
-    except sr.UnknownValueError:
-        print("Erro")
+recognize_from_microphone()
+
+
+
