73d0f8aa9e8157b2065c2751a208501a838392f6
André <andre.alves.silvazesez@gmail.com>
2024-07-25 11:32:12 -0300

    refactor: remove unused variables

--- text-To-Speech.py
+++ text-To-Speech.py
@@ -1,7 +1,6 @@
 
 import os
 import io
-from dotenv import load_dotenv
 from pydub import AudioSegment
 from pydub.utils import which
 from pydub.playback import play
@@ -13,18 +12,11 @@ def play_audio_from_bytes(audio_data, format='wav'):
         audio_segment = AudioSegment.from_file(audio_data_io, format=format)
         play(audio_segment)
 
-
 ### Configurando o caminho do ffmpeg para o pydub
 ffmpeg_path = os.path.join('bin', 'ffmpeg.exe')
 ffplay_path = os.path.join('bin', 'ffplay.exe')
 AudioSegment.converter = ffmpeg_path
 AudioSegment.ffplay = ffplay_path
-
-##### Carregnado variável de ambiente 
-load_dotenv()
-api_key = os.getenv('api_key')
-endpoint = os.getenv('endpoint')
-region = os.getenv('region')
 
 ###### Configuração da Azure
 speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('api_key'), region=os.environ.get('region'))
