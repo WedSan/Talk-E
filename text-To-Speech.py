8a2d379314b255bf80ae981a2d0eea5a7d34c529
André <andre.alves.silvazesez@gmail.com>
2024-07-25 18:12:38 -0300

    fix: ensure audio doesn't play more than once

--- text-To-Speech.py
+++ text-To-Speech.py
@@ -12,12 +12,6 @@ def play_audio_from_bytes(audio_data, format='wav'):
         audio_segment = AudioSegment.from_file(audio_data_io, format=format)
         play(audio_segment)
 
-### Configurando o caminho do ffmpeg para o pydub
-ffmpeg_path = os.path.join('bin', 'ffmpeg.exe')
-ffplay_path = os.path.join('bin', 'ffplay.exe')
-AudioSegment.converter = ffmpeg_path
-AudioSegment.ffplay = ffplay_path
-
 ###### Configuração da Azure
 speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('AZURE_SPEECH_KEY'), region=os.environ.get('AZURE_SPEECH_REGION'))
 audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
@@ -34,12 +28,9 @@ text = """Seja bem vindo ao Talk-e, sua IA para prática de Inglês. Welcome to 
 # Síntese de fala 
 speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
 
-
 # Verificar o resultado da síntese
 if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
     print("Síntese de fala concluída com sucesso.")
-    audio_data = speech_synthesis_result.audio_data
-    play_audio_from_bytes(audio_data)
 elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
     cancellation_details = speech_synthesis_result.cancellation_details
     print(f"Síntese de fala cancelada: {cancellation_details.reason}")
